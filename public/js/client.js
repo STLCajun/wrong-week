9b96743bae3243c2f8aff09d4df15033f2c11543
Luther Lowry <LutherMac@gmail.com>
2017-03-21 15:31:50 -0500

    Basic socket.io implementation - it's not realtime, but it's still pretty cool

---
+++ public/js/client.js
@@ -0,0 +1,29 @@
+var socket = io();
+
+socket.on('connect', function() {
+   socket.emit('join', {}, function(err) {
+       if (err) {
+           alert(err);
+       } else {
+           console.log('Connected');
+       }
+   })
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newTweet', function(tweet) {
+    var template = $('#bubble-content-template').html();
+    var html = Mustache.render(template, {
+        text: tweet.text,
+        user: {
+            name: tweet.user.name,
+            screen_name: tweet.user.screen_name,
+            location: tweet.user.location,
+            profile_image_url: tweet.user.profile_image_url
+        }
+    });
+    $('.bubble').html(html);
+});
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 require('./config/config');
+const http = require('http');
 const express = require('express');
-const app = express();
-const hbs = require('hbs');
 const {Tweets} = require('./utils/tweets');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const hbs = require('hbs');
 
 const port = process.env.PORT || 3000;
-hbs.registerPartials(__dirname + '/views/partials');
-var tweets = new Tweets();
-
-// Run Immediate Check for Tweets
-tweets.checkTweets();
-
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+
+var tweets = new Tweets();
 
 app.get('/', (req, res) => {
     res.render('homepage', tweets.getTweets());
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    socket.on('join', (params, callback) => {
+        console.log('user connected');
+    });
+
+    io.emit('newTweet', tweets.getTweets());
+
+    setInterval(() => {
+        io.emit('newTweet', tweets.getTweets());
+    }, 30000);
+
+    socket.on('disconnect', (params, callback) => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
